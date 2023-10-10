542c303604b6d40b6f188a8ca97b398a0cf40b51
herveperchec <herve.perchec@gmail.com>
2023-10-10 19:56:54 +0200

    build: add release cli command

--- bin/cli/cmds/release.js
+++ bin/cli/cmds/release.js
@@ -1,11 +1,12 @@
 const path = require('path')
 const semver = require('semver')
+const prompts = require('prompts')
+const execa = require('execa')
 
 const rootDir = process.cwd()
 
-// // Get package.json content
-// const packageJson = require(path.resolve(rootDir, './package.json'))
-// const updateVersion = require('../lib/version/update-version')
+// Get package.json content
+const packageJson = require(path.resolve(rootDir, './package.json'))
 
 // Exports command object
 const command = {
@@ -32,6 +33,10 @@ const command = {
       type: 'string',
       describe: 'Pre-release id',
       requiresArg: true
+    }).option('dry', {
+      type: 'boolean',
+      describe: 'Run in dry mode',
+      default: false
     })
   },
   /**
@@ -40,160 +45,124 @@ const command = {
    * @return {void}
    */
   handler: async function (argv) {
-  //   let targetVersion
-  //   const currentVersion = packageJson.version
-  //   const packageName = packageJson.name
-  //   const preId = argv.preid || (semver.prerelease(currentVersion) && semver.prerelease(currentVersion)[0])
-  //   const inc = i => semver.inc(currentVersion, i, preId)
-  //   const versionIncrements = [
-  //     'patch',
-  //     'minor',
-  //     'major',
-  //     ...(preId ? [ 'prepatch', 'preminor', 'premajor', 'prerelease' ] : [])
-  //   ]
+    let targetVersion
+    const currentVersion = packageJson.version
+    const packageName = packageJson.name
+    const preId = argv.preid || (semver.prerelease(currentVersion) && semver.prerelease(currentVersion)[0])
+    const inc = i => semver.inc(currentVersion, i, preId)
+    const versionIncrements = [
+      'patch',
+      'minor',
+      'major',
+      ...(preId ? [ 'prepatch', 'preminor', 'premajor', 'prerelease' ] : [])
+    ]
 
-  //   /**
-  //    * First, check if local repository is clean
-  //    */
-  //   step('Checking changes to commit')
-  //   const { stdout } = await run('git', [ 'diff' ], { stdio: 'pipe', cwd: rootDir })
-  //   if (stdout) {
-  //     error('Please commit your changes before creating a new release!', new Error('There are changes to commit'))
-  //   }
-  //   substep(chalk.green('✔ Local repository is clean'), { last: true })
-  //   log() // Blank line
+    /**
+     * First, check if local repository is clean
+     */
+    console.log('Checking changes to commit')
+    const { stdout } = await execa('git',
+      [ 'diff' ],
+      { stdio: 'pipe', cwd: rootDir }
+    )
+    if (stdout) {
+      console.error('Please commit your changes before creating a new release!', new Error('There are changes to commit'))
+      process.exit(1)
+    }
+    console.log(chalk.green('✔ Local repository is clean'))
+    console.log() // Blank line
 
-  //   /**
-  //    * Release prompt
-  //    */
-  //   step('Setup')
-  //   const { release } = await prompts([
-  //     {
-  //       type: 'select',
-  //       name: 'release',
-  //       message: 'Release type:',
-  //       choices: versionIncrements.map(i => ({ title: `${i} (${inc(i)})`, value: inc(i) })).concat([ { title: 'custom', value: 'custom' } ])
-  //     }
-  //   ])
-  //   // If custom release
-  //   if (release === 'custom') {
-  //     const { version: customVersion } = await prompts([
-  //       {
-  //         type: 'text',
-  //         name: 'version',
-  //         message: 'New custom version:',
-  //         initial: currentVersion,
-  //         validate: value => Boolean(semver.valid(value))
-  //       }
-  //     ])
-  //     targetVersion = customVersion
-  //   } else {
-  //     targetVersion = release
-  //   }
+    /**
+     * Release prompt
+     */
+    console.log('Setup')
+    const { release } = await prompts([
+      {
+        type: 'select',
+        name: 'release',
+        message: 'Release type:',
+        choices: versionIncrements.map(i => ({ title: `${i} (${inc(i)})`, value: inc(i) })).concat([ { title: 'custom', value: 'custom' } ])
+      }
+    ])
+    // If custom release
+    if (release === 'custom') {
+      const { version: customVersion } = await prompts([
+        {
+          type: 'text',
+          name: 'version',
+          message: 'New custom version:',
+          initial: currentVersion,
+          validate: value => Boolean(semver.valid(value))
+        }
+      ])
+      targetVersion = customVersion
+    } else {
+      targetVersion = release
+    }
 
-  //   /**
-  //    * Demand confirmation
-  //    */
-  //   const { yes } = await prompts([
-  //     {
-  //       type: 'confirm',
-  //       name: 'yes',
-  //       message: `Releasing v${targetVersion}. Confirm?`,
-  //       initial: true
-  //     }
-  //   ])
-  //   if (!yes) {
-  //     abort()
-  //     return // exit if not confirmed
-  //   }
-  //   log() // Blank line
+    /**
+     * Demand confirmation
+     */
+    const { yes } = await prompts([
+      {
+        type: 'confirm',
+        name: 'yes',
+        message: `Releasing v${targetVersion}. Confirm?`,
+        initial: true
+      }
+    ])
+    if (!yes) {
+      process.exit()
+      return // exit if not confirmed
+    }
+    console.log() // Blank line
 
-  //   /**
-  //    * Run tests
-  //    */
-  //   // step(`Running tests`)
-  //   // ... here run tests
+    /**
+     * Update version in necessary files
+     */
+    console.log('Bump version')
+    await execa('npm',
+      [
+        'version',
+        (argv.dry ? '--no-git-tag-version' : [])
+      ],
+      { stdio: 'pipe', cwd: rootDir }
+    )
+    console.log() // Blank line
 
-  //   /**
-  //    * Update version in necessary files
-  //    */
-  //   await updateVersion(targetVersion)
-  //   log() // Blank line
+    /**
+     * Publish package (skip in dry mode)
+     */
+    if (!argv.dry) {
+      console.log(`Publishing ${packageName}`)
+      const releaseTag = targetVersion.includes('alpha')
+        ? 'alpha'
+        : targetVersion.includes('beta')
+          ? 'beta'
+          : targetVersion.includes('rc')
+            ? 'rc'
+            : null
+      let alreadyPublished = false
+      try {
+        await execa('npm', [ 'publish', ...(releaseTag ? [ '--tag', releaseTag ] : []) ], { stdio: 'pipe', cwd: rootDir })
+      } catch (e) {
+        if (e.stderr.match(/previously published/)) {
+          alreadyPublished = true
+        } else {
+          error('Unknown error during publishing', e)
+        }
+      }
 
-  //   /**
-  //    * Generate changelog file
-  //    */
-  //   step('Generating changelog')
-  //   await wait('Generating', async () => {
-  //     await run('node', [ './bin/cli', 'changelog' ], { stdio: 'pipe', cwd: rootDir })
-  //   })
-  //   substep(chalk.green('✔ Success'), { last: true })
-  //   log() // Blank line
+      console.log(
+        alreadyPublished
+          ? chalk.yellow(`Skipping already published: ${packageName}`)
+          : chalk.green('✔ Success')
+      )
+      console.log() // Blank line
+    }
 
-  //   /**
-  //    * Publish package
-  //    */
-  //   step(`Publishing ${packageName}`)
-  //   const releaseTag = targetVersion.includes('alpha')
-  //     ? 'alpha'
-  //     : targetVersion.includes('beta')
-  //       ? 'beta'
-  //       : targetVersion.includes('rc')
-  //         ? 'rc'
-  //         : null
-  //   let alreadyPublished = false
-  //   await wait('Publishing', async () => {
-  //     try {
-  //       await run('npm', [ 'publish', ...(releaseTag ? [ '--tag', releaseTag ] : []) ], { stdio: 'pipe', cwd: rootDir })
-  //     } catch (e) {
-  //       if (e.stderr.match(/previously published/)) {
-  //         alreadyPublished = true
-  //       } else {
-  //         error('Unknown error during publishing', e)
-  //       }
-  //     }
-  //   })
-  //   substep(
-  //     alreadyPublished
-  //       ? chalk.yellow(`Skipping already published: ${packageName}`)
-  //       : chalk.green('✔ Success'),
-  //     { last: true }
-  //   )
-  //   log() // Blank line
-
-  //   /**
-  //    * Push to git
-  //    */
-  //   step('Pushing changes')
-  //   await wait('Committing', async () => {
-  //     try {
-  //       await run('git', [ 'add', '.' ], { stdio: 'pipe', cwd: rootDir })
-  //       await run('git', [ 'commit', '-m', `release: v${targetVersion}` ], { stdio: 'pipe', cwd: rootDir })
-  //     } catch (e) {
-  //       error('Unable to commit', e)
-  //     }
-  //   })
-  //   substep(chalk.green('✔ Committed'))
-  //   await wait('Creating tag', async () => {
-  //     try {
-  //       await run('git', [ 'tag', '-a', `v${targetVersion}`, '-m', `v${targetVersion}` ], { stdio: 'pipe', cwd: rootDir })
-  //     } catch (e) {
-  //       error('Unable to create tag', e)
-  //     }
-  //   })
-  //   substep(chalk.green('✔ Tagged'))
-  //   await wait('Pushing', async () => {
-  //     try {
-  //       await run('git', [ 'push', 'origin', '--follow-tags' ], { stdio: 'pipe', cwd: rootDir })
-  //     } catch (e) {
-  //       error('Unable to push', e)
-  //     }
-  //   })
-  //   substep(chalk.green('✔ Pushed'), { last: true })
-  //   log() // blank line
-
-  //   log(chalk.green(`✔ Release v${targetVersion} successfuly created`))
-  //   log()
+    log(chalk.green(`✔ Release v${targetVersion} successfuly created`))
+    log()
   }
 }
 
