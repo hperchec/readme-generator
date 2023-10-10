cae6d00b7fbbe28ea2b8479461098b2798e45350
herveperchec <herve.perchec@gmail.com>
2023-10-10 18:36:01 +0200

    style: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,60 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true
-  },
   extends: [
-    'standard'
+    'standard',
+    'plugin:jsdoc/recommended'
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parserOptions: {
-    ecmaVersion: 2018
-  },
+  processor: 'disable/disable', // For "disable" plugin
+  plugins: [
+    'disable', // plugin to disable other plugins (https://www.npmjs.com/package/eslint-plugin-disable)
+    'jsdoc' // parse docblocks (JSDoc)
+  ],
   rules: {
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'array-bracket-spacing': [ 'error', 'always' ],
+    // eslint-plugin-jsdoc
+    'jsdoc/check-tag-names': [ 'error', {
+      definedTags: [
+        'category', // jsdoc2md/dmd featured tag
+        'typicalname' // jsdoc2md/dmd featured tag
+      ]
+    } ]
+  },
+  settings: {
+    /**
+     * Global settings for eslint-plugin-jsdoc
+     */
+    jsdoc: {
+      tagNamePreference: {
+        return: 'returns',
+        augments: 'extends'
+      }
+    }
+  },
+  overrides: [
+    /**
+     * eslint-plugin-disable:
+     * Use "jsdoc" plugin only for source files, disable for other
+     */
+    {
+      files: [
+        '!src/**/*.js'
+      ],
+      settings: {
+        'disable/plugins': [ 'jsdoc' ]
+      }
+    },
+    /**
+     * Tests environment
+     */
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
