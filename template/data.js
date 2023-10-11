3a4155cea477b9584b56c608c1059767daa07347
herveperchec <herve.perchec@gmail.com>
2023-10-11 22:18:03 +0200

    style: remove comments

--- template/data.js
+++ template/data.js
@@ -4,12 +4,10 @@
 
 // Import package.json data
 const pkg = require('../../package.json')
-// Repository URL
+
 const repositoryUrl = pkg.repository.url
 const projectUrl = pkg.repository.url.match(/^[git+]?(.*)\.git$/)[1] // find string between 'git+' and '.git'
-// Get dependencies
 const dependencies = pkg.dependencies || {}
-// Get dev dependencies
 const devDependencies = pkg.devDependencies || {}
 
 /**
@@ -21,5 +19,4 @@ module.exports = {
   projectUrl,
   dependencies,
   devDependencies
-  /* ... */
 }
